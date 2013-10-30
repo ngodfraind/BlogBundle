a75a9653089b3803d320feed5c8465c077d2a11d
Maxime AILLOUD <maxime.ailloud@gmail.com>
2013-10-30 15:46:49 +0100

    Adding field for configuring the blog's banner + displaying banner only if user has the right to

--- Entity/BlogOptions.php
+++ Entity/BlogOptions.php
@@ -63,18 +63,46 @@ class BlogOptions
     protected $autoPublishComment;
 
     /**
+     * @var bool
+     *
+     * @ORM\Column(type="boolean", name="banner_activate", options={"default" = true})
+     */
+    protected $bannerActivate;
+
+    /**
      * @var string
      *
-     * @ORM\Column(type="string", name="banner_background_color")
+     * @ORM\Column(type="string", name="banner_background_color", options={"default" = "white"})
      */
     protected $bannerBackgroundColor;
 
     /**
      * @var integer
      *
-     * @ORM\Column(type="smallint", name="banner_height")
+     * @ORM\Column(type="smallint", name="banner_height", options={"default" = 100})
      */
     protected $bannerHeight;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", name="banner_image", nullable=true)
+     */
+    protected $bannerImage;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(type="smallint", name="banner_image_position", nullable=true)
+     */
+    protected $bannerImagePosition;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(type="smallint", name="banner_image_repeat", nullable=true)
+     */
+    protected $bannerImageRepeat;
 
     public function __construct()
     {
@@ -213,4 +241,123 @@ class BlogOptions
         return $this->postPerPage;
     }
 
+    /**
+     * @param string $bannerBackgroundColor
+     *
+     * @return BlogOptions
+     */
+    public function setBannerBackgroundColor($bannerBackgroundColor)
+    {
+        $this->bannerBackgroundColor = $bannerBackgroundColor;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBannerBackgroundColor()
+    {
+        return $this->bannerBackgroundColor;
+    }
+
+    /**
+     * @param int $bannerHeight
+     *
+     * @return BlogOptions
+     */
+    public function setBannerHeight($bannerHeight)
+    {
+        $this->bannerHeight = $bannerHeight;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getBannerHeight()
+    {
+        return $this->bannerHeight;
+    }
+
+    /**
+     * @param string $bannerImage
+     *
+     * @return BlogOptions
+     */
+    public function setBannerImage($bannerImage)
+    {
+        $this->bannerImage = $bannerImage;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBannerImage()
+    {
+        return $this->bannerImage;
+    }
+
+    /**
+     * @param int $bannerImagePosition
+     *
+     * @return BlogOptions
+     */
+    public function setBannerImagePosition($bannerImagePosition)
+    {
+        $this->bannerImagePosition = $bannerImagePosition;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getBannerImagePosition()
+    {
+        return $this->bannerImagePosition;
+    }
+
+    /**
+     * @param int $bannerImageRepeat
+     *
+     * @return BlogOptions
+     */
+    public function setBannerImageRepeat($bannerImageRepeat)
+    {
+        $this->bannerImageRepeat = $bannerImageRepeat;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getBannerImageRepeat()
+    {
+        return $this->bannerImageRepeat;
+    }
+
+    /**
+     * @param boolean $bannerActivate
+     *
+     * @return BlogOptions
+     */
+    public function setBannerActivate($bannerActivate)
+    {
+        $this->bannerActivate = $bannerActivate;
+
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isBannerActivate()
+    {
+        return $this->bannerActivate;
+    }
 }
